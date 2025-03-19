5cb0bdf9d70bcbdbf3f5d8e57031e85c2c86e287
FatahChan <ahmadfathallah89@gmail.com>
2025-03-19 22:02:40 +0200

    fix: move components, sorted todos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,41 @@
+import { GitHubCorner } from "@/components/github-corner";
+import { CheckIcon } from "@/components/icons/check-icon";
+import { TrashIcon } from "@/components/icons/trash-icon";
+import { UndoIcon } from "@/components/icons/undo-icon";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { addTodo, deleteTodo, todos$, toggleDone } from "@/lib/SupaLegend";
 import { cn } from "@/lib/utils";
-import { observer, use$ } from "@legendapp/state/react";
+import { use$ } from "@legendapp/state/react";
 import { createFileRoute } from "@tanstack/react-router";
 
-const TodoPage = observer(() => {
-  const todos = use$(() => Object.values(todos$.get() ?? {}));
+export const Route = createFileRoute("/")({
+  component: TodoPage,
+});
+
+function TodoPage() {
+  const todos = use$(() => {
+    const todos = Object.values(todos$.get() ?? {});
+    const sortedTodos = todos.sort((a, b) => {
+      if (a.created_at && b.created_at) {
+        return b.created_at.localeCompare(a.created_at);
+      }
+      return 0;
+    });
+    return sortedTodos;
+  });
 
   return (
     <div className="bg-gray-100 dark:bg-gray-800 min-h-screen">
+      <a
+        href="https://github.com/FatahChan/todo-legend-state-supabase"
+        className="github-corner"
+        aria-label="View source on GitHub"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHubCorner />
+      </a>
       <main className="max-w-4xl mx-auto p-6">
         <h1 className="text-3xl font-semibold text-center mb-6">Todo App</h1>
         <p className="text-gray-400 text-center my-2">
@@ -83,70 +109,4 @@ const TodoPage = observer(() => {
       </main>
     </div>
   );
-});
-
-function CheckIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <title>Check</title>
-      <path d="M20 6 9 17l-5-5" />
-    </svg>
-  );
 }
-
-function TrashIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <title>Trash</title>
-      <path d="M3 6h18" />
-      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-    </svg>
-  );
-}
-
-function UndoIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <title>Undo</title>
-      <path d="M3 7v6h6" />
-      <path d="M21 17a9 9 0 0 0-9-9 9 9 0 0 0-6 2.3L3 13" />
-    </svg>
-  );
-}
-export const Route = createFileRoute("/")({
-  component: TodoPage,
-});
